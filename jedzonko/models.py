fcfc36a22ee141eb0bcf2abd1d939d460f31c15d
Syllvvja02 <mikolajczykdrazeksylwia@gmail.com>
2022-09-27 14:33:53 +0200

    model Recipe

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+from django.db.models import BigAutoField
+from django.db.models.functions import datetime
+from django.utils import timezone
+from django.utils.datetime_safe import date
 
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=64)
+    ingredients = models.TextField()
+    description = models.TextField()
+    created = models.DateField(auto_now_add=False, default=date.today)
+    updated = models.DateField(auto_now=False, default=date.today)
+    preparation_time = models.IntegerField()
+    votes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'Nazwa:{self.name}, Składniki:{self.ingredients}, Opis:{self.description}, Utworzono:{self.created},' \
+               f'Zmodyfikowano:{self.updated}, czas przygotowania: {self.preparation_time} Oceny: {self.votes}.'
+
+
