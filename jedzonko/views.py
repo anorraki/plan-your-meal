24f895d5313c12bc28c86e4ea2fe9982982610c1
Syllvvja02 <mikolajczykdrazeksylwia@gmail.com>
2022-09-28 15:29:47 +0200

    karuzela

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,13 +1,25 @@
+import random
 from datetime import datetime
-
 from django.shortcuts import render
 from django.views import View
+
+from jedzonko.models import Recipe
 
 
 class IndexView(View):
 
     def get(self, request):
-        ctx = {"actual_date": datetime.now()}
+        all_recipes = list(Recipe.objects.all())
+        random.shuffle(all_recipes)
+        mixed_recipes = all_recipes[0:3]
+        m1 = mixed_recipes[0]
+        m2 = mixed_recipes[1]
+        m3 = mixed_recipes[2]
+        ctx = {"actual_date": datetime.now(),
+               'm1': m1,
+               'm2': m2,
+               'm3': m3
+               }
         return render(request, "index.html", ctx)
 
 
@@ -58,4 +70,6 @@ class EditPlanView(View):
 class AddRecipeToPlanView(View):
     def get(self, request):
         return render(request, 'app-schedules-meal-recipe.html')
+
+
 
