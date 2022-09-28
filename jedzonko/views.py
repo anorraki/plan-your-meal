4874e3c21511b63e34b153fee2cd06aea39480e0
Grzegorz <pieczaragrzegorz@gmail.com>
2022-09-28 17:21:55 +0200

    zrobione

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from django.core import paginator
+from django.core.paginator import Paginator
 
 from django.shortcuts import render
 from django.views import View
@@ -26,7 +26,10 @@ class RecipeDetailView(View):
 
 class RecipesView(View):
     def get(self, request):
-        recipes = Recipe.objects.all().order_by('votes').order_by('created')
+        recipes_lists = Recipe.objects.all().order_by('votes').order_by('created')
+        paginator = Paginator(recipes_lists, 1)
+        page = request.GET.get('page')
+        recipes = paginator.get_page(page)
         return render(request, 'app-recipes.html', {'recipes': recipes})
 
 
